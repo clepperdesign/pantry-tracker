5b650c8933a6ef0bda878559ac6d5af9300106ed
Coffee Skeleton <33584689+coffeeSkeleton@users.noreply.github.com>
2019-03-19 11:58:43 -0700

    Made a lot of changes!
    -updates database text file
    - removed 'store' attribute from Grocery class, updated buydate attribute
    - finally got dates importing/exporting properly and have initial code set up for converting time strings back to datetime objects so timedelta calculations can be run on them
    - hallelujah

--- pantry.py
+++ pantry.py
@@ -1,23 +1,58 @@
 ## dicts are grocery items, keys are name, cost, store, price, date of purchase, and date of final use(eaten/used for last time); still working on last one
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta, datetime
 #custom class
 class Grocery():
-	def __init__(self,name,store,price=0,buydate= date.today()):
-		self.name = name
-		self.store = store
-		self.price = price
+	def __init__(self,buydate,name,price=0,):
 		self.buydate = buydate
+		self.name = name
+		self.price = price
+		
 
 importList = []
 #import from file as lists, to a list
 with open('pantry.txt','r') as imp:
         for line in imp:
-                importList.append(eval((line),{"__builtins__":None},{})) #making sure eval can't be abused
+                importList.append(eval((line))) # security bit was causing some trouble, will add back in once the main code is working consistently
 
 pantryList = [Grocery(*x) for x in importList] #found this on stackexchange https://stackoverflow.com/questions/19307169/how-to-assign-a-class-object-to-a-item-in-a-list-python
 
+def addItem():
+        today = date.today().strftime('%m-%d-%y') 
+        toadd = str.lower(input('Add item from purchase? y/n \n'))
+        newitem = []
+        if toadd == 'y' or toadd == 'yes':
+                dateask = str.lower(input('Item purchased today? y/n \n'))
+                if dateask == 'y' or dateask == 'yes':
+                        dateassign = today
+                        newitem.append(dateassign)
+                else:
+                        print('this feature not active yet') #convert string to iso??
+                itemask = str.lower(input('Enter item name. \n'))
+                newitem.append(itemask)
+                try:
+                        priceask = int(input('Enter item price. \n'))
+                        newitem.append(priceask)
+                except:
+                        print('Invalid price entered. \n')
+        else:
+                print('bye')
+        print(newitem)
+        pantryList.append(newitem)
+        print(pantryList)
+        def updatelist():
+                f = open('pantry.txt', 'w')
+                for item in pantryList:
+                        f.write(str(item)+'\n') #allows dictionaries to be written to file properly
+                f.close()
+        updatelist()
+
+addItem()
+
 #testing
-print(importList)
-print(pantryList)
-print(pantryList[0].name,pantryList[0].buydate)
+today = date.today().strftime('%m-%d-%y') 
+todaycalc = datetime.strptime(today, '%m-%d-%y') #converts time string back to datetime component
+oneday = timedelta(days=1)
+
+pantry0date = datetime.strptime(pantryList[0].buydate, '%m-%d-%y')
+print(pantry0date)
+print(pantry0date - oneday)
